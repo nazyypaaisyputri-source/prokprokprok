b26471bcb5601cb636112cad73c4761c7b8d38d0
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-03-11 10:45:59 +0700

    Menambahkan prosedur masukanData untuk input berat dan tinggi

--- prokprok.cpp
+++ prokprok.cpp
@@ -2,3 +2,12 @@
 #include <string>
 
 using namespace std;
+
+// 1. Prosedur untuk input data
+// Menggunakan '&' supaya nilai berat & tinggi yang diisi di sini bisa terbaca di main
+void masukanData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
